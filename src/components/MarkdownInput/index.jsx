a2c662710eecdd3c0e90510df45625f404db15fb
Septentrion50 <erwan56lecorre@gmail.com>
2021-08-05 10:08:40 +0200

    Implement controlled component

--- src/components/MarkdownInput/index.jsx
+++ src/components/MarkdownInput/index.jsx
@@ -2,23 +2,39 @@ import React from 'react';
 
 
 const MarkdownInput = () => {
-    return (<>
-      <form className="note-form mt-5">
-        <div className="mb-3">
-          <input type="text" 
-                  className="form-control" 
-                  id="titleInput" 
-                  placeholder="Ma première note"/>
-        </div>
-        <div className="mb-3">
-          <textarea className="form-control" 
-                    id="descTextarea"></textarea>
-        </div>
-        <div className="mb-3">
-          <button type="submit" className="btn btn-danger">Sauvegarder</button>
-        </div>
-      </form>
-    </>)
+
+  const [title, setTitle] = React.useState('');
+  const [text, setText] = React.useState('');
+
+  const onTitleChange = (e) => {
+    setTitle(e.target.value);
+  }
+  
+  const onTextChange = (e) => {
+    setText(e.target.value);
+  }
+
+  return (<>
+    <form className="note-form mt-5">
+      <div className="mb-3">
+        <input type="text" 
+                className="form-control" 
+                id="titleInput" 
+                placeholder="Ma première note"
+                value={title}
+                onChange={onTitleChange}/>
+      </div>
+      <div className="mb-3">
+        <textarea className="form-control" 
+                  id="descTextarea"
+                  value={text}
+                  onChange={onTextChange}></textarea>
+      </div>
+      <div className="mb-3">
+        <button type="submit" className="btn btn-danger">Sauvegarder</button>
+      </div>
+    </form>
+  </>)
 }
 
 export default MarkdownInput
