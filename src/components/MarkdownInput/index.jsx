3d9b2e30db1248717f5261cd7bdeb71995dbbae8
phbord <phbord@gmail.com>
2021-08-05 10:15:42 +0200

    maj

--- src/components/MarkdownInput/index.jsx
+++ src/components/MarkdownInput/index.jsx
@@ -2,23 +2,36 @@ import React from 'react';
 
 
 const MarkdownInput = () => {
-    return (<>
-      <form className="note-form mt-5">
-        <div className="mb-3">
-          <input type="text" 
-                  className="form-control" 
-                  id="titleInput" 
-                  placeholder="Ma première note"/>
-        </div>
-        <div className="mb-3">
-          <textarea className="form-control" 
-                    id="descTextarea"></textarea>
-        </div>
-        <div className="mb-3">
-          <button type="submit" className="btn btn-danger">Sauvegarder</button>
-        </div>
-      </form>
-    </>)
+  const [title, setTitle] = React.useState('')
+  const [text, setText] = React.useState('')
+
+  const onTitleChange = (e) => {
+    setTitle(e.target.value);
+  };
+  const onTextChange = (e) => {
+    setText(e.target.value);
+  };
+
+  return (<>
+    <form className="note-form mt-5">
+      <div className="mb-3">
+        <input type="text" 
+                value={title} 
+                onChange={onTitleChange}
+                className="form-control" 
+                id="titleInput" 
+                placeholder="Ma première note"/>
+      </div>
+      <div className="mb-3">
+        <textarea className="form-control" 
+                  onChange={onTextChange} 
+                  id="descTextarea">{text}</textarea>
+      </div>
+      <div className="mb-3">
+        <button type="submit" className="btn btn-danger">Sauvegarder</button>
+      </div>
+    </form>
+  </>)
 }
 
 export default MarkdownInput
--- src/components/NoteDisplay/index.jsx
+++ src/components/NoteDisplay/index.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 
 const NoteDisplay = () => {
-    return (<>
-      <div className="note-display"></div>
-    </>)
+  return (<>
+    <div className="note-display"></div>
+  </>)
 }
 
 export default NoteDisplay
